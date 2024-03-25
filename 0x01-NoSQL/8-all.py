ed8d10a679ef3f004f7731fb9607e328a110d19c
LionelMv <mvangicode@gmail.com>
2024-03-25 23:45:18 +0300

    update: make function more efficient
    Checking for empty collection first reduces overhead.

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -9,7 +9,6 @@ def list_all(mongo_collection):
     Returns a list of documents in the collection given,
     or an empty list if no documents are found.
     """
-    if mongo_collection:
-        return [doc for doc in mongo_collection.find()]
-    else:
+    if not mongo_collection:
         return []
+    return [doc for doc in mongo_collection.find()]
